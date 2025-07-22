d383d602a0e7e3cf7288515e1f1bf48defb12712
iamachromebookuser2 <c0d3br3ak3r123@gmail.com>
2025-07-21 20:08:58 -0400

    Component.java

--- src/main/java/me/alpha432/oyvey/features/gui/Component.java
+++ src/main/java/me/alpha432/oyvey/features/gui/Component.java
@@ -15,8 +15,7 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Component
-        extends Feature {
+public class Component extends Feature {
     public static int[] counter1 = new int[]{1};
     protected DrawContext context;
     private final List<Item> items = new ArrayList<>();
@@ -29,7 +28,6 @@ public class Component
     private int height;
     private boolean open;
     private boolean hidden = false;
-    private float fullHeight = Float.NaN;
 
     public Component(String name, int x, int y, boolean open) {
         super(name);
@@ -45,9 +43,7 @@ public class Component
     }
 
     private void drag(int mouseX, int mouseY) {
-        if (!this.drag) {
-            return;
-        }
+        if (!this.drag) return;
         this.x = this.x2 + mouseX;
         this.y = this.y2 + mouseY;
     }
@@ -57,30 +53,50 @@ public class Component
         this.drag(mouseX, mouseY);
         counter1 = new int[]{1};
         float totalItemHeight = this.open ? this.getTotalItemHeight() - 2.0f : 0.0f;
-        int color = ColorUtil.toARGB(ClickGui.getInstance().topRed.getValue(), ClickGui.getInstance().topGreen.getValue(), ClickGui.getInstance().topBlue.getValue(), 255);
-        context.fill(this.x, this.y - 1, this.x + this.width, this.y + this.height - 6, ClickGui.getInstance().rainbow.getValue() ? ColorUtil.rainbow(ClickGui.getInstance().rainbowHue.getValue()).getRGB() : color);
+
+        int color = ColorUtil.toARGB(
+                ClickGui.getInstance().topRed.getValue(),
+                ClickGui.getInstance().topGreen.getValue(),
+                ClickGui.getInstance().topBlue.getValue(),
+                255
+        );
+
+        int headerColor = ClickGui.getInstance().rainbow.getValue()
+                ? ColorUtil.rainbow(ClickGui.getInstance().rainbowHue.getValue()).getRGB()
+                : color;
+
+        // Draw header background
+        context.fill(this.x, this.y - 1, this.x + this.width, this.y + this.height - 6, headerColor);
+
         if (this.open) {
-            RenderUtil.rect(context.getMatrices(), this.x, (float) this.y + 12.5f, this.x + this.width, (float) (this.y + this.height) + totalItemHeight, 0x77000000);
+            // Draw panel background with transparency
+            RenderUtil.rect(context.getMatrices(), this.x, this.y + 12.5f, this.x + this.width, this.y + this.height + totalItemHeight, 0x77000000);
         }
-        drawString(this.getName(), (float) this.x + 3.0f, (float) this.y - 4.0f - (float) OyVeyGui.getClickGui().getTextOffset(), -1);
+
+        // Draw component name
+        drawString(this.getName(), this.x + 3f, this.y - 4f - OyVeyGui.getClickGui().getTextOffset(), -1);
+
+        // Enable scissor to clip child items drawing
         ScissorUtil.enable(context, x, 0, x + width, mc.getWindow().getScaledHeight());
 
         if (this.open) {
-            float y = (float) (this.getY() + this.getHeight()) - 3.0f;
-            for (Item item : this.getItems()) {
-                Component.counter1[0] = counter1[0] + 1;
+            float y = this.y + this.height - 3f;
+            for (Item item : this.items) {
+                counter1[0]++;
                 if (item.isHidden()) continue;
-                item.setLocation((float) this.x + 2.0f, y);
-                item.setWidth(this.getWidth() - 4);
+                item.setLocation(this.x + 2f, y);
+                item.setWidth(this.width - 4);
+
                 if (item.isHovering(mouseX, mouseY)) {
                     ScissorUtil.disable(context);
                 }
+
                 item.drawScreen(context, mouseX, mouseY, partialTicks);
 
                 if (item.isHovering(mouseX, mouseY)) {
                     ScissorUtil.enable(context);
                 }
-                y += (float) item.getHeight() + 1.5f;
+                y += item.getHeight() + 1.5f;
             }
         }
 
@@ -92,9 +108,7 @@ public class Component
             this.x2 = this.x - mouseX;
             this.y2 = this.y - mouseY;
             OyVeyGui.getClickGui().getComponents().forEach(component -> {
-                if (component.drag) {
-                    component.drag = false;
-                }
+                if (component.drag) component.drag = false;
             });
             this.drag = true;
             return;
@@ -104,103 +118,60 @@ public class Component
             mc.getSoundManager().play(PositionedSoundInstance.master(SoundEvents.UI_BUTTON_CLICK, 1f));
             return;
         }
-        if (!this.open) {
-            return;
-        }
-        this.getItems().forEach(item -> item.mouseClicked(mouseX, mouseY, mouseButton));
+        if (!this.open) return;
+        this.items.forEach(item -> item.mouseClicked(mouseX, mouseY, mouseButton));
     }
 
     public void mouseReleased(int mouseX, int mouseY, int releaseButton) {
-        if (releaseButton == 0) {
-            this.drag = false;
-        }
-        if (!this.open) {
-            return;
-        }
-        this.getItems().forEach(item -> item.mouseReleased(mouseX, mouseY, releaseButton));
+        if (releaseButton == 0) this.drag = false;
+        if (!this.open) return;
+        this.items.forEach(item -> item.mouseReleased(mouseX, mouseY, releaseButton));
     }
 
     public void onKeyTyped(char typedChar, int keyCode) {
-        if (!this.open) {
-            return;
-        }
-        this.getItems().forEach(item -> item.onKeyTyped(typedChar, keyCode));
+        if (!this.open) return;
+        this.items.forEach(item -> item.onKeyTyped(typedChar, keyCode));
     }
 
     public void onKeyPressed(int key) {
-        if (!open) return;
-        this.getItems().forEach(item -> item.onKeyPressed(key));
+        if (!this.open) return;
+        this.items.forEach(item -> item.onKeyPressed(key));
     }
 
     public void addButton(Button button) {
         this.items.add(button);
     }
 
-    public int getX() {
-        return this.x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return this.y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public int getWidth() {
-        return this.width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public int getHeight() {
-        return this.height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public boolean isHidden() {
-        return this.hidden;
-    }
-
-    public void setHidden(boolean hidden) {
-        this.hidden = hidden;
-    }
-
-    public boolean isOpen() {
-        return this.open;
-    }
-
-    public final List<Item> getItems() {
-        return this.items;
-    }
+    public int getX() { return this.x; }
+    public void setX(int x) { this.x = x; }
+    public int getY() { return this.y; }
+    public void setY(int y) { this.y = y; }
+    public int getWidth() { return this.width; }
+    public void setWidth(int width) { this.width = width; }
+    public int getHeight() { return this.height; }
+    public void setHeight(int height) { this.height = height; }
+    public boolean isHidden() { return this.hidden; }
+    public void setHidden(boolean hidden) { this.hidden = hidden; }
+    public boolean isOpen() { return this.open; }
+    public List<Item> getItems() { return this.items; }
 
     public boolean isHovering(int mouseX, int mouseY) {
-        return mouseX >= this.getX() && mouseX <= this.getX() + this.getWidth() && mouseY >= this.getY() && mouseY <= this.getY() + this.getHeight() - (this.open ? 2 : 0);
+        return mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY <= y + height - (open ? 2 : 0);
     }
 
     private float getTotalItemHeight() {
-        float height = 0.0f;
-        for (Item item : this.getItems()) {
-            height += (float) item.getHeight() + 1.5f;
+        float height = 0f;
+        for (Item item : items) {
+            height += item.getHeight() + 1.5f;
         }
         return height;
     }
 
-    protected void drawString(String text, double x, double y, Color color) {
-        drawString(text, x, y, color.hashCode());
-    }
-
     protected void drawString(String text, double x, double y, int color) {
         context.drawTextWithShadow(mc.textRenderer, text, (int) x, (int) y, color);
+    }
+
+    protected void drawString(String text, double x, double y, Color color) {
+        drawString(text, x, y, color.getRGB());
     }
 }
